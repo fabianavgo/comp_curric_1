09ebd351b514fcffd566f2860f4090a137676dae
fabi <fabiana_soleite@yahoo.com.br>
2012-11-19 23:58:51 -0200

    exercicio9_e_formulario

--- exercicio_php/exerc9.php
+++ exercicio_php/exerc9.php
@@ -1,4 +1,5 @@
-<!--  9 - Criar um formulário HTML que tenha os campos abaixo e depois de submeter o formulário através de POST, utilizar o PHP para mostrar todos os dados submetidos:
+<!--  9 - Criar um formulário HTML que tenha os campos abaixo e depois de submeter o formulário através de POST, 
+utilizar o PHP para mostrar todos os dados submetidos:
 a) hidden
 b) text
 c) password
@@ -15,75 +16,70 @@ g) select -->
 	</head>
 		
 		<body>
-			<h1>Exercicio 9</h1>
-				<form action="formulario.php" method="POST">
-											
-						<div class="formulario">
+			<form action="formulario.php" method="POST" onsubmit="return validar();">
+				 <fieldset>
+
+				<h1> Preencha o formulário </h1>		
+					
+						<!--hidden.php --> 
+						<input type="hidden" name="pagina" value="informa"> 
+					
+				
 						<p>
+							<label for="idNome">*Digite seu nome:</label>							
+							<input type="text" name="nome" id="idNome" required="true"><br> <br>
+							<label for="idEmail">*Digite seu Email:</label>							
+							<input type="text" name="email" id="idEmail" required="true"><br> 
+						</p>
 						
-						</p>
-						</div>
-
-
-							<div class="formulario">
-							<p>
-							<form action="text.php" method="post"> 
-							Text nome: <input type=text name=nome><br> 
-							Text Email: <input type=text name=email><br><br> 
-							</form>
-							</p>
-							</div>
-
-								<div class="formulario">
 								<p>
-								<form action="text_area.php" method="post">
-								Text Area: <textarea name=mensagem cols=10 rows=1 ></textarea><br>
-								</form>
+									<label for="idSenha">*Digite uma senha:</label> 
+									<input type="password" name="senha" id="idSenha"> <br>
 								</p>
-								</div>
 
-										<div class="formulario">
-										<p>
-										<form action="checkbox.php" method="post"> 
-										Exemplo com CheckBox:<br> 
-											<input type=checkbox name="numeros[]" value=10> 10<br> 
-											<input type=checkbox name="numeros[]" value=100> 100<br> 
-											<input type=checkbox name="numeros[]" value=1000> 1000<br>
-											<input type=checkbox name="numeros[]" value=10000> 10000<br> 
-											<input type=checkbox name="numeros[]" value=90> 90<br> 
-											<input type=checkbox name="numeros[]" value=50> 50<br> 
-											<input type=checkbox name="numeros[]" value=30> 30<br> 
-											<input type=checkbox name="numeros[]" value=15> 15<br><BR> 
-										</p>
-										</div>
-							
-							<div class="formulario">
+									<label for="idSexo">*Sexo:</label>
+					                <br>
+					                <input type="radio" name="sexo" id="idSexo"> Feminino
+					                <input type="radio" name="sexo" id="idSexo1" >Masculino
+					            
+					                <br>
+
+				
+								<p>
+								<label for="idInteresse" >Qual sua área de interesse?</label> <br> 
+									
+									<input type="checkbox" name="musica" id="idInteresse" >Musica<br> 
+									<input type="checkbox" name="esporte" id="idInteresse" > Esporte<br>
+									<input type="checkbox" name="noticia" id="idInteresse" > Noticias<br>
+									<input type="checkbox" name="internet" id="idInteresse" > Internet<br> 
+									<input type="checkbox" name="entreterimento" id="idInteresse" > Entreterimento<br>  
+								</p>
+																
 							<p>
-								
-								<form action="radio.php" method="post"> 
-									<B>Exemplo como Radio:</B><br> 
-									<input type=radio name=sistema value="t1"> TESTE 1 
-									<input type=radio name=sistema value="t2"> TESTE 2
-									<input type=radio name=sistema value="t3"> TESTE 3
-									<input type=radio name=sistema value="t4"> TESTE 4 <br><br>
+								<label for="idInformacao">Como ficou sabendo desse site:</label><br> 
+								<input type="radio" name="televisao" id="idInformacao">  Televisão<br>
+								<input type="radio" name="jornal" id="idInformacao"> Jornal<br>
+								<input type="radio" name="internet" id="idInformacao"> Internet<br> 
+								<input type="radio" name="amigos" id="idInformacao"> Amigos 
 							</p>
-							</div>
-
-						<div class="formulario">
-						<p>
-						<form action = "select.php" method = "post">
-						Testando a o Select
-						<select name=selecao> 
-						<option value=testes>Teste 1</option> 
-						<option value=teste>Teste 2</option> 
-						<option value=test>Teste 3</option> </select><BR><BR>
-
-
-
-						</p>
-						</div>
 							
+						<p>
+							<label for="idAvaliacao">*Como você avalia o site?</label> <br>
+							<select required="isSelected"> 
+							<option name="inicial"><label for="idAvaliacao" ></label></option> 
+							<option name="pessimo"><label for="idAvaliacao">Péssimo</label></option> 
+							<option name="regular"><label for="idAvaliacao">Regular</label></option> 
+							<option name="bom"><label for="idAvaliacao">Bom</label></option>
+							<option name="otimo"><label for="idAvaliacao">Ótimo</label></option>  
+							</select>
+						</p>
+
+						 <p><label for="idComentario">Deixe aqui seu comentário: </label>
+						<br>
+						<textarea name="comentario" id="idComentario"cols=30 rows=10 ></textarea> <br>
+						<input name="submit" type="submit" value="ENVIAR" class="botao" >
+					</p>
+				</fieldset>	
 			</form>
-		</div>
-	</body>
+		</body>
 </html>
---
+++ exercicio_php/formulario.php
@@ -0,0 +1,22 @@
+<!-- 10 - No formulário do item 9, criar as seguintes validações:
+a) verificar se o campo está vazio, caso verdadeiro informar ao usuário (em campos obrigatórios)
+b) para o campo “email” deve verificar se ele realmente é um e-mail
+c) retirar as tags html, script, php, dos campos com strip_tags()
+d) criptografar o campo password com a função md5() -->
+
+<?php
+	if (isset($_POST["pagina"]) && $_POST["pagina"] == "informa") {
+		echo strip_tags($_POST["nome"])."<br>";
+		if(isset($_POST["sexo"])) {
+			echo "Feminino <br>";
+		} else {
+			echo "Masculino <br>";
+		}
+		
+	} else{
+
+		header("Location: exerc9.php");
+		echo "Todos os campos devem ser preenchidos.";
+	}
+?>
+<a href="exerc9.php"><h3><b>Voltar<b></h3></a>
